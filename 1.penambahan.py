f4aa866e6f57d15060d4dc2eb522060cd46c5c8d
Miaw Code <bararaff@gmail.com>
2024-09-15 13:52:16 +0700

    commit pertama

---
+++ 1.penambahan.py
@@ -0,0 +1,12 @@
+num1 = 12
+num2 = 3
+
+sum = num1 + num2
+print('Hasil penambahan', num1, 'dan', num2, 'adalah', sum)
+
+#dengan input
+val1 = input('Masukkan value 1 = ')
+val2 = input('Masukkan value 2 = ')
+
+sum2 = int(val1) + int(val2)
+print('Hasilnya', sum2)
